55451942b8a1c3f63597e9cc49854a3170442b42
christianwade <bism.normalizer@gmail.com>
2017-03-17 02:18:05 +0000

    hdpi12.1

--- BismNormalizer/TabularCompare/UI/Connections.Designer.cs
+++ BismNormalizer/TabularCompare/UI/Connections.Designer.cs
@@ -65,11 +65,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cboSourceDatabase.FormattingEnabled = true;
-            this.cboSourceDatabase.Location = new System.Drawing.Point(22, 75);
-            this.cboSourceDatabase.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.cboSourceDatabase.Location = new System.Drawing.Point(11, 39);
             this.cboSourceDatabase.MaxDropDownItems = 11;
             this.cboSourceDatabase.Name = "cboSourceDatabase";
-            this.cboSourceDatabase.Size = new System.Drawing.Size(422, 33);
+            this.cboSourceDatabase.Size = new System.Drawing.Size(213, 21);
             this.cboSourceDatabase.TabIndex = 12;
             this.cboSourceDatabase.Enter += new System.EventHandler(this.cboSourceDatabase_Enter);
             // 
@@ -79,31 +78,28 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cboSourceServer.FormattingEnabled = true;
-            this.cboSourceServer.Location = new System.Drawing.Point(22, 13);
-            this.cboSourceServer.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.cboSourceServer.Location = new System.Drawing.Point(11, 7);
             this.cboSourceServer.MaxDropDownItems = 11;
             this.cboSourceServer.Name = "cboSourceServer";
-            this.cboSourceServer.Size = new System.Drawing.Size(422, 33);
+            this.cboSourceServer.Size = new System.Drawing.Size(213, 21);
             this.cboSourceServer.TabIndex = 9;
             this.cboSourceServer.TextChanged += new System.EventHandler(this.cboSourceServer_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 208);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(8, 108);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 25);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 11;
             this.label2.Text = "Database";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 146);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(6, 76);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 25);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 10;
             this.label1.Text = "Server";
             // 
@@ -114,10 +110,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlSourceDb.Controls.Add(this.cboSourceServer);
             this.pnlSourceDb.Controls.Add(this.cboSourceDatabase);
-            this.pnlSourceDb.Location = new System.Drawing.Point(134, 127);
-            this.pnlSourceDb.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pnlSourceDb.Location = new System.Drawing.Point(67, 66);
             this.pnlSourceDb.Name = "pnlSourceDb";
-            this.pnlSourceDb.Size = new System.Drawing.Size(466, 127);
+            this.pnlSourceDb.Size = new System.Drawing.Size(233, 66);
             this.pnlSourceDb.TabIndex = 1;
             // 
             // grpSource
@@ -130,11 +125,9 @@
             this.grpSource.Controls.Add(this.label1);
             this.grpSource.Controls.Add(this.label2);
             this.grpSource.Controls.Add(this.pnlSourceDb);
-            this.grpSource.Location = new System.Drawing.Point(24, 23);
-            this.grpSource.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.grpSource.Location = new System.Drawing.Point(12, 12);
             this.grpSource.Name = "grpSource";
-            this.grpSource.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.grpSource.Size = new System.Drawing.Size(610, 263);
+            this.grpSource.Size = new System.Drawing.Size(305, 137);
             this.grpSource.TabIndex = 16;
             this.grpSource.TabStop = false;
             this.grpSource.Text = "Source";
@@ -145,10 +138,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlSourceProject.Controls.Add(this.cboSourceProject);
-            this.pnlSourceProject.Location = new System.Drawing.Point(134, 17);
-            this.pnlSourceProject.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pnlSourceProject.Location = new System.Drawing.Point(67, 9);
             this.pnlSourceProject.Name = "pnlSourceProject";
-            this.pnlSourceProject.Size = new System.Drawing.Size(466, 65);
+            this.pnlSourceProject.Size = new System.Drawing.Size(233, 34);
             this.pnlSourceProject.TabIndex = 18;
             // 
             // cboSourceProject
@@ -159,21 +151,19 @@
             this.cboSourceProject.DisplayMember = "Name";
             this.cboSourceProject.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboSourceProject.FormattingEnabled = true;
-            this.cboSourceProject.Location = new System.Drawing.Point(22, 13);
-            this.cboSourceProject.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.cboSourceProject.Location = new System.Drawing.Point(11, 7);
             this.cboSourceProject.MaxDropDownItems = 11;
             this.cboSourceProject.Name = "cboSourceProject";
-            this.cboSourceProject.Size = new System.Drawing.Size(422, 33);
+            this.cboSourceProject.Size = new System.Drawing.Size(213, 21);
             this.cboSourceProject.TabIndex = 9;
             this.cboSourceProject.SelectedIndexChanged += new System.EventHandler(this.cboSourceProject_SelectedIndexChanged);
             // 
             // rdoSourceProject
             // 
             this.rdoSourceProject.AutoSize = true;
-            this.rdoSourceProject.Location = new System.Drawing.Point(14, 37);
-            this.rdoSourceProject.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.rdoSourceProject.Location = new System.Drawing.Point(7, 19);
             this.rdoSourceProject.Name = "rdoSourceProject";
-            this.rdoSourceProject.Size = new System.Drawing.Size(110, 29);
+            this.rdoSourceProject.Size = new System.Drawing.Size(58, 17);
             this.rdoSourceProject.TabIndex = 17;
             this.rdoSourceProject.Text = "Project";
             this.rdoSourceProject.UseVisualStyleBackColor = true;
@@ -183,10 +173,9 @@
             // 
             this.rdoSourceDb.AutoSize = true;
             this.rdoSourceDb.Checked = true;
-            this.rdoSourceDb.Location = new System.Drawing.Point(14, 88);
-            this.rdoSourceDb.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.rdoSourceDb.Location = new System.Drawing.Point(7, 46);
             this.rdoSourceDb.Name = "rdoSourceDb";
-            this.rdoSourceDb.Size = new System.Drawing.Size(135, 29);
+            this.rdoSourceDb.Size = new System.Drawing.Size(71, 17);
             this.rdoSourceDb.TabIndex = 16;
             this.rdoSourceDb.TabStop = true;
             this.rdoSourceDb.Text = "Database";
@@ -203,11 +192,9 @@
             this.grpTarget.Controls.Add(this.label3);
             this.grpTarget.Controls.Add(this.label4);
             this.grpTarget.Controls.Add(this.pnlTargetDb);
-            this.grpTarget.Location = new System.Drawing.Point(730, 23);
-            this.grpTarget.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.grpTarget.Location = new System.Drawing.Point(365, 12);
             this.grpTarget.Name = "grpTarget";
-            this.grpTarget.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.grpTarget.Size = new System.Drawing.Size(610, 263);
+            this.grpTarget.Size = new System.Drawing.Size(305, 137);
             this.grpTarget.TabIndex = 17;
             this.grpTarget.TabStop = false;
             this.grpTarget.Text = "Target";
@@ -218,10 +205,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlTargetProject.Controls.Add(this.cboTargetProject);
-            this.pnlTargetProject.Location = new System.Drawing.Point(134, 17);
-            this.pnlTargetProject.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pnlTargetProject.Location = new System.Drawing.Point(67, 9);
             this.pnlTargetProject.Name = "pnlTargetProject";
-            this.pnlTargetProject.Size = new System.Drawing.Size(464, 65);
+            this.pnlTargetProject.Size = new System.Drawing.Size(232, 34);
             this.pnlTargetProject.TabIndex = 18;
             // 
             // cboTargetProject
@@ -232,20 +218,18 @@
             this.cboTargetProject.DisplayMember = "Name";
             this.cboTargetProject.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboTargetProject.FormattingEnabled = true;
-            this.cboTargetProject.Location = new System.Drawing.Point(22, 13);
-            this.cboTargetProject.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.cboTargetProject.Location = new System.Drawing.Point(11, 7);
             this.cboTargetProject.MaxDropDownItems = 11;
             this.cboTargetProject.Name = "cboTargetProject";
-            this.cboTargetProject.Size = new System.Drawing.Size(422, 33);
+            this.cboTargetProject.Size = new System.Drawing.Size(213, 21);
             this.cboTargetProject.TabIndex = 9;
             // 
             // rdoTargetProject
             // 
             this.rdoTargetProject.AutoSize = true;
-            this.rdoTargetProject.Location = new System.Drawing.Point(14, 37);
-            this.rdoTargetProject.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.rdoTargetProject.Location = new System.Drawing.Point(7, 19);
             this.rdoTargetProject.Name = "rdoTargetProject";
-            this.rdoTargetProject.Size = new System.Drawing.Size(110, 29);
+            this.rdoTargetProject.Size = new System.Drawing.Size(58, 17);
             this.rdoTargetProject.TabIndex = 17;
             this.rdoTargetProject.Text = "Project";
             this.rdoTargetProject.UseVisualStyleBackColor = true;
@@ -255,10 +239,9 @@
             // 
             this.rdoTargetDb.AutoSize = true;
             this.rdoTargetDb.Checked = true;
-            this.rdoTargetDb.Location = new System.Drawing.Point(14, 88);
-            this.rdoTargetDb.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.rdoTargetDb.Location = new System.Drawing.Point(7, 46);
             this.rdoTargetDb.Name = "rdoTargetDb";
-            this.rdoTargetDb.Size = new System.Drawing.Size(135, 29);
+            this.rdoTargetDb.Size = new System.Drawing.Size(71, 17);
             this.rdoTargetDb.TabIndex = 16;
             this.rdoTargetDb.TabStop = true;
             this.rdoTargetDb.Text = "Database";
@@ -268,20 +251,18 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 146);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(6, 76);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 25);
+            this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Server";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 208);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Location = new System.Drawing.Point(6, 108);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(104, 25);
+            this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "Database";
             // 
@@ -292,10 +273,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlTargetDb.Controls.Add(this.cboTargetServer);
             this.pnlTargetDb.Controls.Add(this.cboTargetDatabase);
-            this.pnlTargetDb.Location = new System.Drawing.Point(134, 127);
-            this.pnlTargetDb.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pnlTargetDb.Location = new System.Drawing.Point(67, 66);
             this.pnlTargetDb.Name = "pnlTargetDb";
-            this.pnlTargetDb.Size = new System.Drawing.Size(464, 127);
+            this.pnlTargetDb.Size = new System.Drawing.Size(232, 66);
             this.pnlTargetDb.TabIndex = 15;
             // 
             // cboTargetServer
@@ -304,11 +284,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cboTargetServer.FormattingEnabled = true;
-            this.cboTargetServer.Location = new System.Drawing.Point(22, 13);
-            this.cboTargetServer.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.cboTargetServer.Location = new System.Drawing.Point(11, 7);
             this.cboTargetServer.MaxDropDownItems = 11;
             this.cboTargetServer.Name = "cboTargetServer";
-            this.cboTargetServer.Size = new System.Drawing.Size(422, 33);
+            this.cboTargetServer.Size = new System.Drawing.Size(213, 21);
             this.cboTargetServer.TabIndex = 9;
             this.cboTargetServer.TextChanged += new System.EventHandler(this.cboTargetServer_TextChanged);
             // 
@@ -318,11 +297,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cboTargetDatabase.FormattingEnabled = true;
-            this.cboTargetDatabase.Location = new System.Drawing.Point(22, 75);
-            this.cboTargetDatabase.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.cboTargetDatabase.Location = new System.Drawing.Point(11, 39);
             this.cboTargetDatabase.MaxDropDownItems = 11;
             this.cboTargetDatabase.Name = "cboTargetDatabase";
-            this.cboTargetDatabase.Size = new System.Drawing.Size(422, 33);
+            this.cboTargetDatabase.Size = new System.Drawing.Size(213, 21);
             this.cboTargetDatabase.TabIndex = 12;
             this.cboTargetDatabase.Enter += new System.EventHandler(this.cboTargetDatabase_Enter);
             // 
@@ -330,10 +308,9 @@
             // 
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(1028, 306);
-            this.btnOK.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnOK.Location = new System.Drawing.Point(506, 159);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(150, 44);
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
             this.btnOK.TabIndex = 18;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -343,10 +320,9 @@
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(1190, 306);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnCancel.Location = new System.Drawing.Point(587, 159);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(150, 44);
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 19;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -356,10 +332,9 @@
             this.btnSwitch.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnSwitch.BackgroundImage = global::BismNormalizer.Resources.ButtonSwitch;
             this.btnSwitch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btnSwitch.Location = new System.Drawing.Point(646, 94);
-            this.btnSwitch.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSwitch.Location = new System.Drawing.Point(323, 50);
             this.btnSwitch.Name = "btnSwitch";
-            this.btnSwitch.Size = new System.Drawing.Size(72, 62);
+            this.btnSwitch.Size = new System.Drawing.Size(36, 32);
             this.btnSwitch.TabIndex = 20;
             this.btnSwitch.UseVisualStyleBackColor = true;
             this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
@@ -367,17 +342,16 @@
             // Connections
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(1368, 373);
+            this.ClientSize = new System.Drawing.Size(682, 194);
             this.Controls.Add(this.btnSwitch);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.grpTarget);
             this.Controls.Add(this.grpSource);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Connections";
